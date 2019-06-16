ad9c475087a2697beb2aed91fed1c01c7e4008a9
Agnieszka Iwanaszko <a.iwanaszko@gmail.com>
2019-06-16 16:18:40 +0200

    Add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import MainLayout from './components/layout/MainLayout/MainLayout';
+import Home from './components/views/Home/Home';
+import About from './components/views/About/About';
+import Projects from './components/views/Projects/Projects';
+import Contact from './components/views/Contact/Contact';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
+      <MainLayout>
+      <Switch>
+        <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
+        <Route path={process.env.PUBLIC_URL + '/about'} component={About} />
+        <Route exact path={process.env.PUBLIC_URL + '/projects'} component={Projects} />
+        <Route path={process.env.PUBLIC_URL + '/Contact'} component={Contact} />
+        </Switch>
+      </MainLayout>
+      </BrowserRouter>
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
