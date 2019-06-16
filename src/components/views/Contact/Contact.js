059f7be7e5d1d5d4c3f6f671bf7245703920632d
Agnieszka Iwanaszko <a.iwanaszko@gmail.com>
2019-06-16 18:22:01 +0200

    Add basic structure and styles to Contact

--- src/components/views/Contact/Contact.js
+++ src/components/views/Contact/Contact.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Contact.scss';
 
 const Contact = () => (
-  <div>
-    <h2>Contact view</h2>
+  <div className='container'>
+  <div className='contact'>
+    <h2>Formularz kontaktowy</h2>
+    <h5>Imie</h5>
+    <input type='text' />
+    <h5>Nazwisko</h5>
+    <input type='text' />
+    <h5>Adres e-mail</h5>
+    <input type='text' />
+    <h5>Tresc zapytania</h5>
+    <input type='text' />
+    <div class='button'>Wyslij</div>
+    </div>
   </div>
 );
 
