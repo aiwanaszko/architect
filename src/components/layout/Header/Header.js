a55da518c78a16e0777957dabdbdc5e4ed767dfb
Agnieszka Iwanaszko <a.iwanaszko@gmail.com>
2019-06-16 17:09:53 +0200

    Add Services link to Header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -7,6 +7,7 @@ class Header extends React.Component {
     <nav>
       <NavLink exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active' className='link' >Home</NavLink>
       <NavLink to={`${process.env.PUBLIC_URL}/about`} activeClassName='active' className='link'>About</NavLink>
+      <NavLink to={`${process.env.PUBLIC_URL}/services`} activeClassName='active' className='link'>Services</NavLink>
       <NavLink to={`${process.env.PUBLIC_URL}/Projects`} activeClassName='active' className='link'>Projects</NavLink>
       <NavLink to={`${process.env.PUBLIC_URL}/contact`} activeClassName='active' className='link'>Contact</NavLink>
     </nav>
